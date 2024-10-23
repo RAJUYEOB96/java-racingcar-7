7b7e630cd787852cc269ce7779a6c62a10d0de67
RAJUYEOB <pag1yun99@gmail.com>
2024-10-23 14:21:07 +0900

    feat: 자동차 이름, 시도 횟수 설정을 위한 입력 기능 추가.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+//import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Arrays;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+        String [] carsName = Console.readLine().trim().split(",");
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String tryNum = Console.readLine();
+        int intTryNum = Integer.parseInt(tryNum);
+
+        System.out.println("cars name : " + Arrays.toString(carsName) + " TryNum : " + intTryNum);
+
     }
 }
